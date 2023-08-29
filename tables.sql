b70aea78727230d7e4a5a05bcbd1422da2ea41bf
victormusembi11 <victor.musembi11@gmail.com>
2023-08-29 17:36:55 +0300

    create table using another table

--- tables.sql
+++ tables.sql
@@ -20,3 +20,29 @@ DESCRIBE Employee;
 -- | email_address | varchar(150) | YES  |     | NULL    |       |
 -- | salary        | int          | YES  |     | NULL    |       |
 -- +---------------+--------------+------+-----+---------+-------+
+
+-- Creating a table using another table (borrowing all fields from Employee table)
+CREATE TABLE Manager AS SELECT * FROM Employee;
+
+-- table description:
+-- +---------------+--------------+------+-----+---------+-------+
+-- | Field         | Type         | Null | Key | Default | Extra |
+-- +---------------+--------------+------+-----+---------+-------+
+-- | emp_id        | int          | YES  |     | NULL    |       |
+-- | first_name    | varchar(100) | YES  |     | NULL    |       |
+-- | last_name     | varchar(100) | YES  |     | NULL    |       |
+-- | email_address | varchar(150) | YES  |     | NULL    |       |
+-- | salary        | int          | YES  |     | NULL    |       |
+-- +---------------+--------------+------+-----+---------+-------+
+
+
+-- Creating a table using another table (borrowing some fields from Employee table)
+CREATE TABLE Manager AS SELECT email_address, salary FROM Employee;
+
+-- table description:
+-- +---------------+--------------+------+-----+---------+-------+
+-- | Field         | Type         | Null | Key | Default | Extra |
+-- +---------------+--------------+------+-----+---------+-------+
+-- | email_address | varchar(150) | YES  |     | NULL    |       |
+-- | salary        | int          | YES  |     | NULL    |       |
+-- +---------------+--------------+------+-----+---------+-------+
