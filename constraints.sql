f49d684b2cb73f817b86a72d989bcf00a2c0d039
victormusembi11 <victor.musembi11@gmail.com>
2023-08-29 19:13:01 +0300

    not null

--- constraints.sql
+++ constraints.sql
@@ -6,3 +6,14 @@ CREATE TABLE Employee (
     email_address VARCHAR(150) NOT NULL,
     salary INT
 );
+
+-- table description (default is null):
+-- +---------------+--------------+------+-----+---------+-------+
+-- | Field         | Type         | Null | Key | Default | Extra |
+-- +---------------+--------------+------+-----+---------+-------+
+-- | emp_id        | int          | NO   |     | NULL    |       |
+-- | first_name    | varchar(100) | NO   |     | NULL    |       |
+-- | last_name     | varchar(100) | NO   |     | NULL    |       |
+-- | email_address | varchar(150) | NO   |     | NULL    |       |
+-- | salary        | int          | YES  |     | NULL    |       |
+-- +---------------+--------------+------+-----+---------+-------+
