6da1c70443160cc6d5d51fbec0124e20627bc2ab
victormusembi11 <victor.musembi11@gmail.com>
2023-08-29 19:12:12 +0300

    not null

---
+++ constraints.sql
@@ -0,0 +1,8 @@
+-- Required fields (NOT NULL)
+CREATE TABLE Employee (
+    emp_id INT NOT NULL,
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    email_address VARCHAR(150) NOT NULL,
+    salary INT
+);
