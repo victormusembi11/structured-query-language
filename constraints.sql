56fde9456851701adb6d55624c813a468beba86e
victormusembi11 <victor.musembi11@gmail.com>
2023-08-29 23:14:40 +0300

    primary key constraint

--- constraints.sql
+++ constraints.sql
@@ -62,3 +62,29 @@ ALTER TABLE Manager DROP INDEX UC_Manager;
 --     email_address VARCHAR(255) NOT NULL,
 --     CONSTRAINT UC_Manager UNIQUE (manager_id, email_address)
 -- );
+
+
+-- Primary Key
+-- The primary key constraint uniquely each record in a table
+-- A table can only have one primary key
+
+-- Method 1
+CREATE TABLE Manager (
+    manager_id INT NOT NULL PRIMARY KEY,
+    email_address VARCHAR(255) NOT NULL
+);
+
+-- Method 2
+CREATE TABLE Manager (
+    manager_id INT NOT NULL,
+    email_address VARCHAR(255) NOT NULL,
+    PRIMARY KEY (manager_id)
+);
+
+-- table description:
+-- +---------------+--------------+------+-----+---------+-------+
+-- | Field         | Type         | Null | Key | Default | Extra |
+-- +---------------+--------------+------+-----+---------+-------+
+-- | manager_id    | int          | NO   | PRI | NULL    |       |
+-- | email_address | varchar(255) | NO   |     | NULL    |       |
+-- +---------------+--------------+------+-----+---------+-------+
